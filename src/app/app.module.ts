f18990025be671459dd359254c68cc5d7e1e16e3
muriithiderro <muriithiderrick56@gmail.com>
2018-05-01 18:20:23 +0300

    added more contents into the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpModule} from "@angular/http"
+import {ProfileService} from "./services/profile.service"
+import {FormsModule} from '@angular/forms'
+import {Routes,RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {HomeComponent} from './components/home/home.component';
+import { FilterPipe } from './pipe/filter.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
+
+
+const appRoutes:Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user', component: ProfileComponent},
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomeComponent,
+    FilterPipe,
+    HighlightDirective,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
