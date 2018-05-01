ead21573ce3da7fb8d1c876d16b7d7d97d69b5e9
muriithiderro <muriithiderrick56@gmail.com>
2018-05-01 18:21:56 +0300

    created two models, user and repository to create instances of User and Repository using data from github api

---
+++ src/app/models/repository-model.ts
@@ -0,0 +1,5 @@
+export class Repo{
+	constructor(public name:string,public url:string,download:string){
+
+	}
+}
---
+++ src/app/models/user-model.ts
@@ -0,0 +1,65 @@
+export class User{
+
+constructor(public login:string,
+	public id:number,
+	public avatar_url:string,
+	public url:string,
+	public html_url:string,
+	public followers_url:string,
+	public following_url:string,
+	public gists_url:string,
+	public starred_url:string,
+	public subscriptions_url:string,
+	public organizations_url:string,
+	public repos_url:string,
+	public name:string,
+	public company:string,
+	public location:string,
+	public email:string,
+	public hireable:boolean,
+	public bio:string,
+	public public_repos:number,
+	public public_gists:number,
+	public followers:number,
+	public following:number
+
+	){}
+ 
+}
+
+
+
+// 	{
+//   "login": "muriithiderro",
+//   "id": 17847647,
+//   "avatar_url": "https://avatars1.githubusercontent.com/u/17847647?v=4",
+//   "gravatar_id": "",
+//   "url": "https://api.github.com/users/muriithiderro",
+//   "html_url": "https://github.com/muriithiderro",
+//   "followers_url": "https://api.github.com/users/muriithiderro/followers",
+//   "following_url": "https://api.github.com/users/muriithiderro/following{/other_user}",
+//   "gists_url": "https://api.github.com/users/muriithiderro/gists{/gist_id}",
+//   "starred_url": "https://api.github.com/users/muriithiderro/starred{/owner}{/repo}",
+//   "subscriptions_url": "https://api.github.com/users/muriithiderro/subscriptions",
+//   "organizations_url": "https://api.github.com/users/muriithiderro/orgs",
+//   "repos_url": "https://api.github.com/users/muriithiderro/repos",
+//   "events_url": "https://api.github.com/users/muriithiderro/events{/privacy}",
+//   "received_events_url": "https://api.github.com/users/muriithiderro/received_events",
+//   "type": "User",
+//   "site_admin": false,
+//   "name": "deryq",
+//   "company": null,
+//   "blog": "",
+//   "location": "embu,kenya",
+//   "email": null,
+//   "hireable": true,
+//   "bio": "am an enthusiast in gis web development and free and open source  stuffs.",
+//   "public_repos": 19,
+//   "public_gists": 0,
+//   "followers": 3,
+//   "following": 6,
+//   "created_at": "2016-03-15T09:19:54Z",
+//   "updated_at": "2018-04-23T19:14:47Z"
+
+
+// }
