5d7c1b311921d453b00a115b8d3a92b561c04fb3
muriithiderro <muriithiderrick56@gmail.com>
2018-05-01 18:10:36 +0300

    created two components home and profile component

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {ProfileService} from '../../services/profile.service'
+import {User} from '../../models/user-model'
+import {Repo} from '../../models/repository-model'
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+ private profile
+ private repos
+ private username:string
+ private repoName:string
+ private resp:any
+ foundRepo
+
+    constructor(private profileservice:ProfileService) { 
+      this.repos = []
+     
+  }
+
+  findProfile(){
+  	this.profileservice.updateProfile(this.username)
+  	this.profileservice.searchUser().then( response => this.profile =response);
+    
+    this.profileservice.getProfileRepos().then((response)=>{
+   	this.repos = response
+     console.log(this.repos)
+   }) 
+    
+  }
+ 
+  ngOnInit() {
+  }
+
+}
